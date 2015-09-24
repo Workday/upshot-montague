df46bd664789cd9580102c59a6b2c079c7513038
aleksandr.nisnevich <aleksandr.nisnevich@workday.com>
2015-09-24 15:39:29 -0700

    ArithmeticParser examples in README

--- src/main/scala/example/ArithmeticParser.scala
+++ src/main/scala/example/ArithmeticParser.scala
@@ -1,9 +1,10 @@
 package example
 
-import ccg.CcgCat
-import parser.{SemanticParseResult, SemanticParser}
+import ccg.{X, N, CcgCat}
+import parser._
+import semantics._
 
-object ArithmeticParser extends SemanticParser[CcgCat](arithmeticLexicon) {
+object ArithmeticParser extends SemanticParser[CcgCat](ArithmeticLexicon.lexicon) {
   def parse(str: String): SemanticParseResult[CcgCat] = parse(str, tokenizer = parenTokenizer)
 
   def main(args: Array[String]): Unit = {
@@ -19,4 +20,20 @@ object ArithmeticParser extends SemanticParser[CcgCat](arithmeticLexicon) {
   private def parenTokenizer(str: String) = {
     str.replace("(", " ( ").replace(")", " ) ").trim.toLowerCase.split("\\s+")
   }
+}
+
+object ArithmeticLexicon {
+  val lexicon =  ParserDict[CcgCat]() +
+    (Seq("+", "plus") -> ((N\N)/N, λ {y: Int => λ {x: Int => x + y}})) +
+    (Seq("-", "minus") -> ((N\N)/N, λ {y: Int => λ {x: Int => x - y}})) +
+    (Seq("*", "times") -> ((N\N)/N, λ {y: Int => λ {x: Int => x * y}})) +
+    (Seq("+/-", "plus/minus") -> Seq( // example of ambiguous definition
+      ((N\N)/N, λ {y: Int => λ {x: Int => x + y}}),
+      ((N\N)/N, λ {y: Int => λ {x: Int => x - y}})
+    )) +
+    ("(" -> (Paren/N, identity)) +
+    (")" -> (N\Paren, identity)) +
+    (IntegerMatcher -> (N, {i: Int => Form(i)})) +  // IntegerMatcher matches using Integer.parseInt
+    (Else -> (X|X, {s: String => identity}))  // Else matches any single tokens that don't match anything else;
+                                              // X|X is the identity CCG category
 }
--- src/main/scala/example/package.scala
+++ src/main/scala/example/package.scala
@@ -4,18 +4,4 @@ import semantics._
 
 package object example {
   case object Paren extends TerminalCat { val category = "Paren" } // syntactic category for parenthetical expressions
-
-  val arithmeticLexicon = ParserDict[CcgCat]() +
-    (Seq("+", "plus") -> ((N\N)/N, λ {y: Int => λ {x: Int => x + y}})) +
-    (Seq("-", "minus") -> ((N\N)/N, λ {y: Int => λ {x: Int => x - y}})) +
-    (Seq("*", "times") -> ((N\N)/N, λ {y: Int => λ {x: Int => x * y}})) +
-    (Seq("+/-", "plus/minus") -> Seq( // example of ambiguous definition
-      ((N\N)/N, λ {y: Int => λ {x: Int => x + y}}),
-      ((N\N)/N, λ {y: Int => λ {x: Int => x - y}})
-    )) +
-    ("(" -> (Paren/N, identity)) +
-    (")" -> (N\Paren, identity)) +
-    (IntegerMatcher -> (N, {i: Int => Form(i)})) +  // IntegerMatcher matches using Integer.parseInt
-    (Else -> (X|X, {s: String => identity}))  // Else matches any single tokens that don't match anything else;
-                                              // X|X is the identity CCG category
 }
